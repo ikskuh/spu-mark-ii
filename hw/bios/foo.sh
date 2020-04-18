59871886bf574fa04291bc062d7e93bec3c9dd5b
Felix (xq) Queißner <git@mq32.de>
2020-04-18 17:22:33 +0200

    Cleans up repository.

--- hw/bios/foo.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-
-for i in {0..63}; do
-	echo $i | sed -E 's/(.*)/#{top.testbench.simulated_ram[\1][15:0]} top.testbench.simulated_ram[\1][15] top.testbench.simulated_ram[\1][14] top.testbench.simulated_ram[\1][13] top.testbench.simulated_ram[\1][12] top.testbench.simulated_ram[\1][11] top.testbench.simulated_ram[\1][10] top.testbench.simulated_ram[\1][9] top.testbench.simulated_ram[\1][8] top.testbench.simulated_ram[\1][7] top.testbench.simulated_ram[\1][6] top.testbench.simulated_ram[\1][5] top.testbench.simulated_ram[\1][4] top.testbench.simulated_ram[\1][3] top.testbench.simulated_ram[\1][2] top.testbench.simulated_ram[\1][1] top.testbench.simulated_ram[\1][0]/g' >> ../hw-impl/testbench.gtkw
-done
